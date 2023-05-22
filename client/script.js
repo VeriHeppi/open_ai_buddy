72e164f673152ab57e7e86ba15b5ffbed5514c35
Roope Mantere <roope.mantere@tuni.fi>
2023-05-23 02:27:40 +0300

    finish backend and frot end connections

--- client/script.js
+++ client/script.js
@@ -74,6 +74,33 @@ const handleSubmit = async (event) => {
 
   loader(messageDiv);
   
+  // fetch data from the server -> bot's response
+
+  const response = await fetch('http://localhost:5000', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 
+      prompt: data.get('prompt') 
+    })
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  }
+  else {
+    const err = await response.json();
+    
+    messageDiv.innerHTML = "Something went wrong!";
+    alert(err);
+  }
 }
 
 form.addEventListener('submit', handleSubmit);
